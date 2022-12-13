ed16497f4dc4c611b8e6ac3aea66a1dabc914ba7
SirJohanot <pavelkrafft@gmail.com>
2022-12-13 11:22:38 +0300

    added more pages; fixed the table creation issue; added the initial implementation for the internationalization

--- src/main/java/com/patiun/libraryspring/configuration/SecurityConfig.java
+++ src/main/java/com/patiun/libraryspring/configuration/SecurityConfig.java
@@ -21,7 +21,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/books", "/home").authenticated()
+                        .requestMatchers("/books", "/").authenticated()
                         .anyRequest().permitAll()
                 )
                 .formLogin((form) -> form

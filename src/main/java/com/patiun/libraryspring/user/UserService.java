6c789e3769eb3320689d144820f21211786d1259
SirJohanot <pavelkrafft@gmail.com>
2022-12-21 17:20:18 +0300

    added user page

--- src/main/java/com/patiun/libraryspring/user/UserService.java
+++ src/main/java/com/patiun/libraryspring/user/UserService.java
@@ -9,6 +9,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.StreamSupport;
 
 @Service
@@ -44,5 +45,9 @@ public class UserService implements UserDetailsService {
     public List<User> getAllUsers() {
         return StreamSupport.stream(userRepository.findAll().spliterator(), false)
                 .toList();
+    }
+
+    public Optional<User> getUserById(Integer id) {
+        return userRepository.findById(id);
     }
 }

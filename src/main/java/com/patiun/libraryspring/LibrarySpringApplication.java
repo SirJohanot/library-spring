69a007c94c65447883baae4ae77dd3a291d64e83
SirJohanot <pavelkrafft@gmail.com>
2023-02-11 10:36:29 +0300

    removed a redundant annotation

--- src/main/java/com/patiun/libraryspring/LibrarySpringApplication.java
+++ src/main/java/com/patiun/libraryspring/LibrarySpringApplication.java
@@ -2,11 +2,8 @@ package com.patiun.libraryspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication
-@ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.REGEX, pattern = "com.patiun.libraryspring.mode.*"))
 public class LibrarySpringApplication {
 
     public static void main(String[] args) {

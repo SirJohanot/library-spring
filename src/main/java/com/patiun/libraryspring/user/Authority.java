c7d52f0ade0b32e2b81b2b59b11fad21a6e4ae59
SirJohanot <pavelkrafft@gmail.com>
2023-02-12 10:23:45 +0300

    added Authority enum

---
+++ src/main/java/com/patiun/libraryspring/user/Authority.java
@@ -0,0 +1,14 @@
+package com.patiun.libraryspring.user;
+
+public enum Authority {
+
+    READ_BOOKS,
+    READ_ORDERS,
+    PLACE_ORDERS,
+    COLLECT_ORDERS,
+    JUDGE_ORDERS,
+    EDIT_BOOKS,
+    ADD_BOOKS,
+    READ_USERS,
+    EDIT_USERS
+}

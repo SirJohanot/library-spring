d0a8a1224d91d78aa67dcf24d4c0995e15902baa
SirJohanot <pavelkrafft@gmail.com>
2023-02-19 10:49:38 +0300

    added tests for the PasswordsMatchValidator

---
+++ src/test/java/com/patiun/libraryspring/validation/PasswordsMatchValidatorTest.java
@@ -0,0 +1,37 @@
+package com.patiun.libraryspring.validation;
+
+import com.patiun.libraryspring.user.UserRegistrationDto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PasswordsMatchValidatorTest {
+
+    private PasswordsMatchValidator validator;
+
+    @BeforeEach
+    public void setup() {
+        validator = new PasswordsMatchValidator();
+    }
+
+    @Test
+    public void isValidShouldReturnTrueWhenPasswordsMatch() {
+        //given
+        UserRegistrationDto user = new UserRegistrationDto("login", "password", "password", "firstName", "lastName");
+        //when
+        boolean isValid = validator.isValid(user, null);
+        //then
+        assertThat(isValid).isTrue();
+    }
+
+    @Test
+    public void isValidaShouldReturnFalseWhenPasswordsDoNotMatch() {
+        //given
+        UserRegistrationDto user = new UserRegistrationDto("login", "password", "differentPassword", "firstName", "lastName");
+        //when
+        boolean isValid = validator.isValid(user, null);
+        //then
+        assertThat(isValid).isFalse();
+    }
+}

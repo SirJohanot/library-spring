3e471e2e11a20eb03242c807f1a24062185360f1
SirJohanot <pavelkrafft@gmail.com>
2023-03-28 12:10:13 +0300

    added an explicit private constructor to the constants-containing Regexp class

--- src/main/java/com/patiun/libraryspring/validation/Regexp.java
+++ src/main/java/com/patiun/libraryspring/validation/Regexp.java
@@ -2,6 +2,9 @@ package com.patiun.libraryspring.validation;
 
 public final class Regexp {
 
+    private Regexp() {
+    }
+
     public static final String HUMAN_NAME = "\\p{L}+.*";
 
     public static final String HUMAN_NAMES_DELIMITED_BY_COMMA = "\\p{L}+.*( *, +\\p{L}+.*)*";

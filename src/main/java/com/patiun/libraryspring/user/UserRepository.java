8fdd3420899dcfb9533863fdc5bd7acbec76ce03
SirJohanot <pavelkrafft@gmail.com>
2023-04-01 12:25:34 +0300

    added UserRepository.findByRoleIs

--- src/main/java/com/patiun/libraryspring/user/UserRepository.java
+++ src/main/java/com/patiun/libraryspring/user/UserRepository.java
@@ -3,10 +3,13 @@ package com.patiun.libraryspring.user;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
 
     Optional<User> findByLogin(String login);
+
+    List<User> findByRoleIs(UserRole role);
 }

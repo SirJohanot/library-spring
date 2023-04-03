a67dec6b8bb31809a3df30375e02d1520b4943d5
SirJohanot <pavelkrafft@gmail.com>
2023-04-03 13:01:22 +0300

    added tests for the PublisherRepository

---
+++ src/test/java/com/patiun/libraryspring/book/PublisherRepositoryTest.java
@@ -0,0 +1,65 @@
+package com.patiun.libraryspring.book;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+public class PublisherRepositoryTest {
+
+    private final TestEntityManager entityManager;
+
+    private final PublisherRepository publisherRepository;
+
+    @Autowired
+    public PublisherRepositoryTest(TestEntityManager entityManager, PublisherRepository publisherRepository) {
+        this.entityManager = entityManager;
+        this.publisherRepository = publisherRepository;
+    }
+
+    @Test
+    public void findByNameShouldReturnOptionalOfThePublisherWithTheNameWhenPublisherExists() {
+        //given
+        entityManager.persist(new Publisher(null, "Hardcover"));
+
+        entityManager.persist(new Publisher(null, "Book Worm"));
+
+        String thirdPublisherName = "Target Publisher";
+        Publisher thirdPublisher = new Publisher(null, thirdPublisherName);
+        entityManager.persist(thirdPublisher);
+
+        entityManager.flush();
+
+        Optional<Publisher> expectedResult = Optional.of(thirdPublisher);
+        //when
+        Optional<Publisher> actualResult = publisherRepository.findOptionalByName(thirdPublisherName);
+        //then
+        assertThat(actualResult)
+                .isEqualTo(expectedResult);
+    }
+
+    @Test
+    public void findByNameShouldReturnEmptyOptionalWhenPublisherDoesNotExist() {
+        //given
+        entityManager.persist(new Publisher(null, "Hardcover"));
+
+        entityManager.persist(new Publisher(null, "Book Worm"));
+
+        entityManager.persist(new Publisher(null, "Target Publisher"));
+
+        entityManager.flush();
+        //when
+        Optional<Publisher> actualResult = publisherRepository.findOptionalByName("Blah blah blah");
+        //then
+        assertThat(actualResult)
+                .isEqualTo(Optional.empty());
+    }
+}

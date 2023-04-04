860f33823c294d4a95221ddacc958473a68a9eed
SirJohanot <pavelkrafft@gmail.com>
2023-04-04 09:30:56 +0300

    added tests for AuthorRepository

---
+++ src/test/java/com/patiun/libraryspring/book/AuthorRepositoryTest.java
@@ -0,0 +1,65 @@
+package com.patiun.libraryspring.book;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+public class AuthorRepositoryTest {
+
+    private final TestEntityManager entityManager;
+
+    private final AuthorRepository authorRepository;
+
+    @Autowired
+    public AuthorRepositoryTest(TestEntityManager entityManager, AuthorRepository authorRepository) {
+        this.entityManager = entityManager;
+        this.authorRepository = authorRepository;
+    }
+
+    @Test
+    public void findByNameShouldReturnOptionalOfTheAuthorWithTheNameWhenAuthorExists() {
+        //given
+        entityManager.persist(new Author("George Orwell"));
+
+        String secondAuthorName = "Kanstantsin Mikhailovich Mitskievich";
+        Author secondAuthor = new Author(secondAuthorName);
+        entityManager.persist(secondAuthor);
+
+        entityManager.persist(new Author("Mikhail Lermontov"));
+
+        entityManager.flush();
+
+        Optional<Author> expectedResult = Optional.of(secondAuthor);
+        //when
+        Optional<Author> actualResult = authorRepository.findOptionalByName(secondAuthorName);
+        //then
+        assertThat(actualResult)
+                .isEqualTo(expectedResult);
+    }
+
+    @Test
+    public void findByNameShouldReturnEmptyOptionalWhenAuthorDoesNotExist() {
+        //given
+        entityManager.persist(new Author("George Orwell"));
+
+        entityManager.persist(new Author("Kanstantsin Mikhailovich Mitskievich"));
+
+        entityManager.persist(new Author("Mikhail Lermontov"));
+
+        entityManager.flush();
+        //when
+        Optional<Author> actualResult = authorRepository.findOptionalByName("Maksim Tank");
+        //then
+        assertThat(actualResult)
+                .isEqualTo(Optional.empty());
+    }
+}

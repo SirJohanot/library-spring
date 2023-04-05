37f82bd900b15111015a48187b1238edd401aace
SirJohanot <pavelkrafft@gmail.com>
2023-04-05 11:00:50 +0300

    added tests for the GenreRepository

---
+++ src/test/java/com/patiun/libraryspring/book/GenreRepositoryTest.java
@@ -0,0 +1,65 @@
+package com.patiun.libraryspring.book;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+public class GenreRepositoryTest {
+
+    private final TestEntityManager entityManager;
+
+    private final GenreRepository genreRepository;
+
+    @Autowired
+    public GenreRepositoryTest(TestEntityManager entityManager, GenreRepository genreRepository) {
+        this.entityManager = entityManager;
+        this.genreRepository = genreRepository;
+    }
+
+    @Test
+    public void findByNameShouldReturnOptionalOfTheGenreWithTheNameWhenGenreExists() {
+        //given
+        String firstGenreName = "Horror";
+        Genre firstGenre = new Genre(null, firstGenreName);
+        entityManager.persist(firstGenre);
+
+        entityManager.persist(new Genre(null, "Fantasy"));
+
+        entityManager.persist(new Genre(null, "Detective"));
+
+        entityManager.flush();
+
+        Optional<Genre> expectedResult = Optional.of(firstGenre);
+        //when
+        Optional<Genre> actualResult = genreRepository.findOptionalByName(firstGenreName);
+        //then
+        assertThat(actualResult)
+                .isEqualTo(expectedResult);
+    }
+
+    @Test
+    public void findByNameShouldReturnEmptyOptionalWhenGenreDoesNotExist() {
+        //given
+        entityManager.persist(new Genre(null, "Horror"));
+
+        entityManager.persist(new Genre(null, "Fantasy"));
+
+        entityManager.persist(new Genre(null, "Detective"));
+
+        entityManager.flush();
+        //when
+        Optional<Genre> actualResult = genreRepository.findOptionalByName("Historical novel");
+        //then
+        assertThat(actualResult)
+                .isEqualTo(Optional.empty());
+    }
+}

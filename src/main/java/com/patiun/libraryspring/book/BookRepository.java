fba4a8b7535dfcaea0e7743447403a4ce676d6f9
SirJohanot <pavelkrafft@gmail.com>
2023-04-06 09:20:10 +0300

    added BookRepository.findAllByIsDeletedFalse

--- src/main/java/com/patiun/libraryspring/book/BookRepository.java
+++ src/main/java/com/patiun/libraryspring/book/BookRepository.java
@@ -2,5 +2,9 @@ package com.patiun.libraryspring.book;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface BookRepository extends CrudRepository<Book, Integer> {
+
+    List<Book> findAllByIsDeletedFalse();
 }

724bdd5b85a05f1aaa12470d9e70162deefcbc84
SirJohanot <pavelkrafft@gmail.com>
2023-04-08 09:27:39 +0300

    changed BookOrderRepository to extend ListCrudRepository

--- src/main/java/com/patiun/libraryspring/order/BookOrderRepository.java
+++ src/main/java/com/patiun/libraryspring/order/BookOrderRepository.java
@@ -1,12 +1,12 @@
 package com.patiun.libraryspring.order;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface BookOrderRepository extends CrudRepository<BookOrder, Integer> {
+public interface BookOrderRepository extends ListCrudRepository<BookOrder, Integer> {
 
     List<BookOrder> findByUserId(Integer id);
 }

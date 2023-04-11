4cb4ec2741e302ee22486f492cda1d9be77124ef
SirJohanot <pavelkrafft@gmail.com>
2023-04-11 10:23:51 +0300

    added a new test to BookOrderRepositoryTest

--- src/test/java/com/patiun/libraryspring/order/BookOrderRepositoryTest.java
+++ src/test/java/com/patiun/libraryspring/order/BookOrderRepositoryTest.java
@@ -66,4 +66,35 @@ public class BookOrderRepositoryTest {
                 .contains(firstOrder, thirdOrder);
     }
 
+    @Test
+    public void testFindByUserIdShouldReturnAnEmptyListWhenSuchOrdersDoNotExist() {
+        //given
+        User firstUser = new User(null, "dok", "shgdfdgsa", "john", "smith", false, UserRole.READER);
+        entityManager.persist(firstUser);
+
+        User secondaryUser = new User(null, "login2", "ihiuehgiwreg2", "fi2rstName", "lastN2ame", false, UserRole.READER);
+        entityManager.persist(secondaryUser);
+
+        Book firstBook = new Book(null, "book2", List.of(new Author("author1")), new Genre(null, "genre2"), new Publisher(null, "publisher2"), 1998, 7, false);
+        entityManager.persist(firstBook);
+
+        Book secondBook = new Book(null, "book3", List.of(new Author("author2")), new Genre(null, "nice genre"), new Publisher(null, "publisher3"), 2000, 65, false);
+        entityManager.persist(secondBook);
+
+        entityManager.flush();
+
+        entityManager.persist(new BookOrder(null, firstBook, firstUser, RentalType.OUT_OF_LIBRARY, LocalDate.of(2023, 3, 22), LocalDate.of(2023, 3, 29), null, OrderState.PLACED));
+
+        entityManager.persist(new BookOrder(null, secondBook, secondaryUser, RentalType.TO_READING_HALL, LocalDate.now(), LocalDate.now(), LocalDate.now(), OrderState.BOOK_RETURNED));
+        
+        entityManager.persist(new BookOrder(null, secondBook, firstUser, RentalType.OUT_OF_LIBRARY, LocalDate.of(2023, 4, 1), LocalDate.of(2023, 4, 8), null, OrderState.BOOK_TAKEN));
+
+        entityManager.flush();
+        //when
+        List<BookOrder> actualResult = orderRepository.findByUserId(20);
+        //then
+        assertThat(actualResult)
+                .isEmpty();
+    }
+
 }

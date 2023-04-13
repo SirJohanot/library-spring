c2bff324263af7ec1a0e9d45037919ed2d02324f
SirJohanot <pavelkrafft@gmail.com>
2023-04-13 14:22:07 +0300

    added a test for UserRestController.authenticate

--- src/test/java/com/patiun/libraryspring/user/UserRestControllerTest.java
+++ src/test/java/com/patiun/libraryspring/user/UserRestControllerTest.java
@@ -8,13 +8,16 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.times;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(UserRestController.class)
 public class UserRestControllerTest {
@@ -54,5 +57,21 @@ public class UserRestControllerTest {
         then(service)
                 .should(times(1))
                 .signUp(login, password, firstName, lastName);
+    }
+
+    @Test
+    @WithMockUser("usernameJohan")
+    public void testAuthenticateShouldReturnTheUsersRolesWhenUserIsFoundByService() throws Exception {
+        //given
+        UserRole role = UserRole.ADMIN;
+
+        given(service.getUserByLogin("usernameJohan"))
+                .willReturn(new User(null, "userNameJohan", "7846578364", "johan", "smith", false, role));
+        //then
+        mvc.perform(get(BASE_URL + "/auth")
+                        .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.roles", hasSize(1)))
+                .andExpect(jsonPath("$.roles", hasItem(role.toString())));
     }
 }

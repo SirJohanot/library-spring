5dfba2763dc42220d4616b752846c55f74a0bf00
SirJohanot <pavelkrafft@gmail.com>
2023-04-26 12:17:51 +0300

    added an integration test for BookRestController.createBook

---
+++ src/test/java/com/patiun/libraryspring/book/BookRestControllerIntegrationTest.java
@@ -0,0 +1,78 @@
+package com.patiun.libraryspring.book;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@Transactional
+public class BookRestControllerIntegrationTest {
+
+    private static final String BASE_URL = "/books";
+
+    private static final String DUMMY_ADMIN_CREDENTIALS = "admin";
+
+    private final MockMvc mvc;
+
+    private final BookRepository bookRepository;
+
+    @Autowired
+    public BookRestControllerIntegrationTest(MockMvc mvc, BookRepository bookRepository) {
+        this.mvc = mvc;
+        this.bookRepository = bookRepository;
+    }
+
+    @Test
+    public void testCreateBookShouldSaveTheNewBookToTheDatabase() throws Exception {
+        //given
+        String title = "Some Book";
+        String authors = "Some Human, Some Non-human";
+        String genre = "Interesting";
+        String publisher = "Smith";
+        Integer publishmentYear = 2014;
+        Integer amount = 10;
+
+        BookEditDto editDto = new BookEditDto(title, authors, genre, publisher, publishmentYear, amount);
+
+        String editDtoJson = new ObjectMapper().writeValueAsString(editDto);
+
+        List<Author> expectedAuthors = Arrays.asList(
+                new Author(1, "Some Human"),
+                new Author(2, "Some Non-human")
+        );
+        Genre expectedGenre = new Genre(1, genre);
+        Publisher expectedPublisher = new Publisher(1, publisher);
+
+        Book expectedBookToBeSaved = new Book(1, title, expectedAuthors, expectedGenre, expectedPublisher, publishmentYear, amount, false);
+        //then
+        mvc.perform(post(BASE_URL)
+                        .with(httpBasic(DUMMY_ADMIN_CREDENTIALS, DUMMY_ADMIN_CREDENTIALS))
+                        .contentType(APPLICATION_JSON)
+                        .content(editDtoJson))
+                .andExpect(status().isOk())
+                .andExpect(content().string(""));
+
+//        assertThat(bookRepository.findAll())
+//                .hasSize(1)
+//                .contains(expectedBookToBeSaved);
+
+        assertThat(bookRepository.findAllByIsDeletedFalse().get(0))
+                .isEqualTo(expectedBookToBeSaved);
+    }
+
+}

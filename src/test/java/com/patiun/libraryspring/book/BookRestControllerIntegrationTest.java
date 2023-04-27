25e52ad343f4806e686123b9e846569106122623
SirJohanot <pavelkrafft@gmail.com>
2023-04-27 09:51:54 +0300

    added the following test: BookRestControllerIntegrationTest.testReadAllBooksShouldReturnTheBookListOfUndeletedBooksWhenNoBooksAreDeleted

--- src/test/java/com/patiun/libraryspring/book/BookRestControllerIntegrationTest.java
+++ src/test/java/com/patiun/libraryspring/book/BookRestControllerIntegrationTest.java
@@ -1,25 +1,32 @@
 package com.patiun.libraryspring.book;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
+@AutoConfigureTestEntityManager
 @Transactional
 public class BookRestControllerIntegrationTest {
 
@@ -31,10 +38,13 @@ public class BookRestControllerIntegrationTest {
 
     private final BookRepository bookRepository;
 
+    private final TestEntityManager testEntityManager;
+
     @Autowired
-    public BookRestControllerIntegrationTest(MockMvc mvc, BookRepository bookRepository) {
+    public BookRestControllerIntegrationTest(MockMvc mvc, BookRepository bookRepository, TestEntityManager testEntityManager) {
         this.mvc = mvc;
         this.bookRepository = bookRepository;
+        this.testEntityManager = testEntityManager;
     }
 
     @Test
@@ -69,6 +79,37 @@ public class BookRestControllerIntegrationTest {
                         Objects.equals(b.getPublishmentYear(), publishmentYear) &&
                         Objects.equals(b.getAmount(), amount)
                 );
+    }
+
+    @Test
+    public void testReadAllBooksShouldReturnTheBookListOfUndeletedBooksWhenNoBooksAreDeleted() throws Exception {
+        //given
+        List<Book> existingBooksOnTheDatabase = Arrays.asList(
+                new Book(null, "book1", List.of(new Author(null, "author1")), new Genre(null, "genre1"), new Publisher(null, "publisher1"), 2003, 12, false),
+                new Book(null, "book2", List.of(new Author(null, "author2")), new Genre(null, "genre2"), new Publisher(null, "publisher2"), 1998, 7, false),
+                new Book(null, "book3", Arrays.asList(new Author(null, "author3"), new Author(null, "author4")), new Genre(null, "genre3"), new Publisher(null, "publisher3"), 2014, 130, false)
+        );
+
+        existingBooksOnTheDatabase = existingBooksOnTheDatabase.stream()
+                .map(testEntityManager::persist)
+                .toList();
+
+        ObjectMapper mapper = new ObjectMapper();
+        String expectedJson = existingBooksOnTheDatabase.stream()
+                .map(u -> {
+                    try {
+                        return mapper.writeValueAsString(u);
+                    } catch (JsonProcessingException e) {
+                        throw new RuntimeException(e);
+                    }
+                })
+                .collect(Collectors.joining(","));
+        expectedJson = "[" + expectedJson + "]";
+        //then
+        mvc.perform(get(BASE_URL)
+                        .with(httpBasic(DUMMY_ADMIN_CREDENTIALS, DUMMY_ADMIN_CREDENTIALS)))
+                .andExpect(status().isOk())
+                .andExpect(content().json(expectedJson));
     }
 
 }

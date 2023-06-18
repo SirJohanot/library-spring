21f85824de99b49c808e3b3f7fc845eb0e6ddba4
SirJohanot <pavelkrafft@gmail.com>
2023-06-18 09:23:55 +0300

    added BookRestControllerIntegrationTest.testCreateBookShouldReturnBadRequestWhenTheBookPublishmentYearIsEarlierThan1900

--- src/test/java/com/patiun/libraryspring/book/BookRestControllerIntegrationTest.java
+++ src/test/java/com/patiun/libraryspring/book/BookRestControllerIntegrationTest.java
@@ -258,6 +258,28 @@ public class BookRestControllerIntegrationTest {
     }
 
     @Test
+    public void testCreateBookShouldReturnBadRequestWhenTheBookPublishmentYearIsEarlierThan1900() throws Exception {
+        //given
+        String title = "Some Book";
+        String authors = "Some Human, Some Non-human";
+        String genre = "Interesting";
+        String publisher = "Smith";
+        Integer publishmentYear = 1899;
+        Integer amount = 10;
+
+        BookEditDto editDto = new BookEditDto(title, authors, genre, publisher, publishmentYear, amount);
+
+        String editDtoJson = new ObjectMapper().writeValueAsString(editDto);
+        //then
+        mvc.perform(post(BASE_URL)
+                        .with(httpBasic(DUMMY_ADMIN_CREDENTIALS, DUMMY_ADMIN_CREDENTIALS))
+                        .contentType(APPLICATION_JSON)
+                        .content(editDtoJson))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.error", any(String.class)));
+    }
+
+    @Test
     public void testReadAllBooksShouldReturnTheBookListOfUndeletedBooksWhenNoExistingBooksAreDeleted() throws Exception {
         //given
         List<Book> existingBooksOnTheDatabase = Arrays.asList(

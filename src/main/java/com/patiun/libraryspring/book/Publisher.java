e4a67429d366b0c8a64009d67f0f7f3802f9c2d0
Johanot <pavelkrafft@gmail.com>
2023-09-07 09:31:56 +0300

    removed the redundant NotBlank annotation from Publisher.name

--- src/main/java/com/patiun/libraryspring/book/Publisher.java
+++ src/main/java/com/patiun/libraryspring/book/Publisher.java
@@ -2,7 +2,6 @@ package com.patiun.libraryspring.book;
 
 import com.patiun.libraryspring.validation.Regexp;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
 import java.util.Objects;
@@ -15,7 +14,6 @@ public class Publisher {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
-    @NotBlank(message = "Publisher name must not be blank")
     @Pattern(regexp = Regexp.WORD, message = "Publisher name must start with an alphabetical character or a number")
     @Column(name = "name", length = 64, unique = true)
     private String name;

7a6336a3b36f02f8133c57b16e7c4f4f61f9c6d2
Johanot <pavelkrafft@gmail.com>
2024-04-30 15:04:09 +0300

    removed the unused HUMAN_NAMES_DELIMITED_BY_COMMA regex

--- src/main/java/com/patiun/libraryspring/validation/Regexp.java
+++ src/main/java/com/patiun/libraryspring/validation/Regexp.java
@@ -7,8 +7,6 @@ public final class Regexp {
 
     public static final String HUMAN_NAME = "\\p{L}+.*";
 
-    public static final String HUMAN_NAMES_DELIMITED_BY_COMMA = "\\p{L}+.*( *, +\\p{L}+.*)*";
-
     public static final String WORD = "[\\p{L}\\w]+.*";
 
     public static final String ISBN = "^([0-9]{10}|[0-9]{13})$";

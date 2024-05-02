b24c70e466f109fc78e884e093909f00b06f5a0a
Johanot <pavelkrafft@gmail.com>
2024-05-02 09:50:26 +0300

    removed validation constraints from User fields

--- src/main/java/com/patiun/libraryspring/user/User.java
+++ src/main/java/com/patiun/libraryspring/user/User.java
@@ -1,11 +1,7 @@
 package com.patiun.libraryspring.user;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.patiun.libraryspring.validation.Regexp;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -20,27 +16,22 @@ public class User implements UserDetails {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
-    @NotBlank(message = "Login must not be blank")
     @Column(name = "login", length = 64, unique = true)
     private String login;
 
-    @NotBlank(message = "Password must not be blank")
     @Column(name = "password", length = 64)
     @JsonIgnore
     private String password;
 
-    @Pattern(regexp = Regexp.HUMAN_NAME, message = "First name must start with an alphabetical character")
     @Column(name = "first_name", length = 64)
     private String firstName;
 
-    @Pattern(regexp = Regexp.HUMAN_NAME, message = "Last name must start with an alphabetical character")
     @Column(name = "last_name", length = 64)
     private String lastName;
 
     @Column(name = "is_blocked")
     private boolean isBlocked;
 
-    @NotNull(message = "Role must not be null")
     @Column(name = "role", length = 64)
     @Enumerated(EnumType.STRING)
     private UserRole role;
@@ -48,7 +39,7 @@ public class User implements UserDetails {
     public User() {
     }
 
-    public User(Integer id, String login, String password, String firstName, String lastName, boolean isBlocked, @NotNull UserRole role) {
+    public User(Integer id, String login, String password, String firstName, String lastName, boolean isBlocked, UserRole role) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
@@ -107,11 +98,11 @@ public class User implements UserDetails {
         isBlocked = blocked;
     }
 
-    public @NotNull UserRole getRole() {
+    public UserRole getRole() {
         return role;
     }
 
-    public void setRole(@NotNull UserRole role) {
+    public void setRole(UserRole role) {
         this.role = role;
     }
 
@@ -161,37 +152,18 @@ public class User implements UserDetails {
         }
 
         User user = (User) o;
-
-        if (!Objects.equals(id, user.id)) {
-            return false;
-        }
-        if (!Objects.equals(login, user.login)) {
-            return false;
-        }
-        if (!Objects.equals(password, user.password)) {
-            return false;
-        }
-        if (!Objects.equals(firstName, user.firstName)) {
-            return false;
-        }
-        if (!Objects.equals(lastName, user.lastName)) {
-            return false;
-        }
-        if (!Objects.equals(isBlocked, user.isBlocked)) {
-            return false;
-        }
-        return role == user.role;
+        return isBlocked == user.isBlocked && Objects.equals(id, user.id) && Objects.equals(login, user.login) && Objects.equals(password, user.password) && Objects.equals(firstName, user.firstName) && Objects.equals(lastName, user.lastName) && role == user.role;
     }
 
     @Override
     public int hashCode() {
-        int result = id != null ? id.hashCode() : 0;
-        result = 31 * result + (login != null ? login.hashCode() : 0);
-        result = 31 * result + (password != null ? password.hashCode() : 0);
-        result = 31 * result + (firstName != null ? firstName.hashCode() : 0);
-        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
-        result = 31 * result + (isBlocked ? 1 : 0);
-        result = 31 * result + role.hashCode();
+        int result = Objects.hashCode(id);
+        result = 31 * result + Objects.hashCode(login);
+        result = 31 * result + Objects.hashCode(password);
+        result = 31 * result + Objects.hashCode(firstName);
+        result = 31 * result + Objects.hashCode(lastName);
+        result = 31 * result + Boolean.hashCode(isBlocked);
+        result = 31 * result + Objects.hashCode(role);
         return result;
     }
 

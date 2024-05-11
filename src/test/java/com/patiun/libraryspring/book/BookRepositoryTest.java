df0ab0f06392f84f19615266f6a77a65428389c2
Johanot <pavelkrafft@gmail.com>
2024-05-11 16:20:51 +0300

    updated BookRepositoryTest.testFindByIdShouldReturnOptionalOfTheBookWithTheIdWhenBookExists

--- src/test/java/com/patiun/libraryspring/book/BookRepositoryTest.java
+++ src/test/java/com/patiun/libraryspring/book/BookRepositoryTest.java
@@ -27,11 +27,11 @@ public class BookRepositoryTest {
     @Test
     public void testFindByIdShouldReturnOptionalOfTheBookWithTheIdWhenBookExists() {
         //given
-        entityManager.persist(new Book(null, "book1", List.of(new Author("author1")), new Genre(null, "genre1"), new Publisher(null, "publisher1"), 2003, "Minsk", "3298614390153", 12, false));
+        entityManager.persist(new Book(null, "book1", List.of(new Author("author1")), List.of(new Editor(null, "editorRole1", "editorName1")), new Genre(null, "genre1"), new Publisher(null, "publisher1", "publisherPostalCode1", "publihserAddress1"), new PrintingHouse(null, "printingHouse1", "printingHousePostalCode1", "printingHouseAddress1"), 2003, "Minsk", "description1", 20, "3298614390153", "80.7", "33.4", 12, false));
 
-        entityManager.persist(new Book(null, "book2", List.of(new Author("author2")), new Genre(null, "genre2"), new Publisher(null, "publisher2"), 1998, "Minsk", "3298614390153", 7, false));
+        entityManager.persist(new Book(null, "book2", List.of(new Author("author2")), List.of(new Editor(null, "editorRole2", "editorName2")), new Genre(null, "genre2"), new Publisher(null, "publisher2", "publisherPostalCode2", "publihserAddress2"), new PrintingHouse(null, "printingHouse2", "printingHousePostalCode2", "printingHouseAddress2"), 1998, "Minsk", "description2", 230, "3298614230153", "81.7", "23.4", 9, false));
 
-        Book thirdBook = new Book(null, "book3", Arrays.asList(new Author("author3"), new Author("author4")), new Genre(null, "genre3"), new Publisher(null, "publisher3"), 2014, "Minsk", "3298614390153", 130, false);
+        Book thirdBook = new Book(null, "book3", List.of(new Author("author3")), List.of(new Editor(null, "editorRole3", "editorName3")), new Genre(null, "genre3"), new Publisher(null, "publisher3", "publisherPostalCode3", "publihserAddress3"), new PrintingHouse(null, "printingHouse3", "printingHousePostalCode3", "printingHouseAddress3"), 1995, "Minsk", "description3", 60, "3123614390153", "70.7", "33.6", 8, false);
         Integer targetBookId = entityManager.persistAndGetId(thirdBook, Integer.class);
 
         entityManager.flush();

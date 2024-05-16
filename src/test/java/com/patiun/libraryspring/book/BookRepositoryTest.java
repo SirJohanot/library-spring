08972e72b6aa6e46462d731f2692786d38c0f39e
Johanot <pavelkrafft@gmail.com>
2024-05-16 09:21:59 +0300

    updated BookRepositoryTest

--- src/test/java/com/patiun/libraryspring/book/BookRepositoryTest.java
+++ src/test/java/com/patiun/libraryspring/book/BookRepositoryTest.java
@@ -26,11 +26,11 @@ public class BookRepositoryTest {
     @Test
     public void testFindByIdShouldReturnOptionalOfTheBookWithTheIdWhenBookExists() {
         //given
-        entityManager.persist(new Book(null, "book1", List.of(new Author("author1")), List.of(new Editor(null, "editorRole1", "editorName1")), new Genre(null, "genre1"), new Publisher(null, "publisher1", "publisherPostalCode1", "publihserAddress1"), new PrintingHouse(null, "printingHouse1", "printingHousePostalCode1", "printingHouseAddress1"), 2003, "Minsk", "description1", 20, "3298614390153", "80.7", "33.4", 12, false));
+        entityManager.persist(new Book(null, "book1", List.of(new Author("author1")), List.of(new Editor(null, "editorRole1", "editorName1")), new Genre(null, "genre1"), new Publisher(null, "publisher1", "publisherPostalCode1", "publihserAddress1"), new PrintingHouse(null, "printingHouse1", "printingHousePostalCode1", "printingHouseAddress1"), 2003, "Minsk", "description1", 20, "3298614390153", "80.7", "33.4", "B1", 12, false));
 
-        entityManager.persist(new Book(null, "book2", List.of(new Author("author2")), List.of(new Editor(null, "editorRole2", "editorName2")), new Genre(null, "genre2"), new Publisher(null, "publisher2", "publisherPostalCode2", "publihserAddress2"), new PrintingHouse(null, "printingHouse2", "printingHousePostalCode2", "printingHouseAddress2"), 1998, "Minsk", "description2", 230, "3298614230153", "81.7", "23.4", 9, false));
+        entityManager.persist(new Book(null, "book2", List.of(new Author("author2")), List.of(new Editor(null, "editorRole2", "editorName2")), new Genre(null, "genre2"), new Publisher(null, "publisher2", "publisherPostalCode2", "publihserAddress2"), new PrintingHouse(null, "printingHouse2", "printingHousePostalCode2", "printingHouseAddress2"), 1998, "Minsk", "description2", 230, "3298614230153", "81.7", "23.4", "B2", 9, false));
 
-        Book thirdBook = new Book(null, "book3", List.of(new Author("author3")), List.of(new Editor(null, "editorRole3", "editorName3")), new Genre(null, "genre3"), new Publisher(null, "publisher3", "publisherPostalCode3", "publihserAddress3"), new PrintingHouse(null, "printingHouse3", "printingHousePostalCode3", "printingHouseAddress3"), 1995, "Minsk", "description3", 60, "3123614390153", "70.7", "33.6", 8, false);
+        Book thirdBook = new Book(null, "book3", List.of(new Author("author3")), List.of(new Editor(null, "editorRole3", "editorName3")), new Genre(null, "genre3"), new Publisher(null, "publisher3", "publisherPostalCode3", "publihserAddress3"), new PrintingHouse(null, "printingHouse3", "printingHousePostalCode3", "printingHouseAddress3"), 1995, "Minsk", "description3", 60, "3123614390153", "70.7", "33.6", "B3", 8, false);
         Integer targetBookId = entityManager.persistAndGetId(thirdBook, Integer.class);
 
         entityManager.flush();
@@ -44,11 +44,11 @@ public class BookRepositoryTest {
     @Test
     public void testFindByIdShouldReturnEmptyOptionalWhenBookDoesNotExist() {
         //given
-        Integer firstBookId = entityManager.persistAndGetId(new Book(null, "book1", List.of(new Author("author1")), List.of(new Editor(null, "editorRole1", "editorName1")), new Genre(null, "genre1"), new Publisher(null, "publisher1", "publisherPostalCode1", "publihserAddress1"), new PrintingHouse(null, "printingHouse1", "printingHousePostalCode1", "printingHouseAddress1"), 2003, "Minsk", "description1", 20, "3298614390153", "80.7", "33.4", 12, false), Integer.class);
+        Integer firstBookId = entityManager.persistAndGetId(new Book(null, "book1", List.of(new Author("author1")), List.of(new Editor(null, "editorRole1", "editorName1")), new Genre(null, "genre1"), new Publisher(null, "publisher1", "publisherPostalCode1", "publihserAddress1"), new PrintingHouse(null, "printingHouse1", "printingHousePostalCode1", "printingHouseAddress1"), 2003, "Minsk", "description1", 20, "3298614390153", "80.7", "33.4", "B1", 12, false), Integer.class);
 
-        Integer secondBookId = entityManager.persistAndGetId(new Book(null, "book2", List.of(new Author("author2")), List.of(new Editor(null, "editorRole2", "editorName2")), new Genre(null, "genre2"), new Publisher(null, "publisher2", "publisherPostalCode2", "publihserAddress2"), new PrintingHouse(null, "printingHouse2", "printingHousePostalCode2", "printingHouseAddress2"), 1998, "Minsk", "description2", 230, "3298614230153", "81.7", "23.4", 9, false), Integer.class);
+        Integer secondBookId = entityManager.persistAndGetId(new Book(null, "book2", List.of(new Author("author2")), List.of(new Editor(null, "editorRole2", "editorName2")), new Genre(null, "genre2"), new Publisher(null, "publisher2", "publisherPostalCode2", "publihserAddress2"), new PrintingHouse(null, "printingHouse2", "printingHousePostalCode2", "printingHouseAddress2"), 1998, "Minsk", "description2", 230, "3298614230153", "81.7", "23.4", "B2", 9, false), Integer.class);
 
-        Integer thirdBookId = entityManager.persistAndGetId(new Book(null, "book3", List.of(new Author("author3")), List.of(new Editor(null, "editorRole3", "editorName3")), new Genre(null, "genre3"), new Publisher(null, "publisher3", "publisherPostalCode3", "publihserAddress3"), new PrintingHouse(null, "printingHouse3", "printingHousePostalCode3", "printingHouseAddress3"), 1995, "Minsk", "description3", 60, "3123614390153", "70.7", "33.6", 8, false), Integer.class);
+        Integer thirdBookId = entityManager.persistAndGetId(new Book(null, "book3", List.of(new Author("author3")), List.of(new Editor(null, "editorRole3", "editorName3")), new Genre(null, "genre3"), new Publisher(null, "publisher3", "publisherPostalCode3", "publihserAddress3"), new PrintingHouse(null, "printingHouse3", "printingHousePostalCode3", "printingHouseAddress3"), 1995, "Minsk", "description3", 60, "3123614390153", "70.7", "33.6", "B3", 8, false), Integer.class);
 
         entityManager.flush();
 
@@ -66,12 +66,12 @@ public class BookRepositoryTest {
     @Test
     public void testFindAllByDeletedFalseShouldReturnAllNonDeletedBooksWhenSuchBooksExist() {
         //given
-        Book firstBook = new Book(null, "book1", List.of(new Author("author1")), List.of(new Editor(null, "editorRole1", "editorName1")), new Genre(null, "genre1"), new Publisher(null, "publisher1", "publisherPostalCode1", "publihserAddress1"), new PrintingHouse(null, "printingHouse1", "printingHousePostalCode1", "printingHouseAddress1"), 2003, "Minsk", "description1", 20, "3298614390153", "80.7", "33.4", 12, false);
+        Book firstBook = new Book(null, "book1", List.of(new Author("author1")), List.of(new Editor(null, "editorRole1", "editorName1")), new Genre(null, "genre1"), new Publisher(null, "publisher1", "publisherPostalCode1", "publihserAddress1"), new PrintingHouse(null, "printingHouse1", "printingHousePostalCode1", "printingHouseAddress1"), 2003, "Minsk", "description1", 20, "3298614390153", "80.7", "33.4", "B1", 12, false);
         entityManager.persist(firstBook);
 
-        entityManager.persist(new Book(null, "book2", List.of(new Author("author2")), List.of(new Editor(null, "editorRole2", "editorName2")), new Genre(null, "genre2"), new Publisher(null, "publisher2", "publisherPostalCode2", "publihserAddress2"), new PrintingHouse(null, "printingHouse2", "printingHousePostalCode2", "printingHouseAddress2"), 1998, "Minsk", "description2", 230, "3298614230153", "81.7", "23.4", 9, true));
+        entityManager.persist(new Book(null, "book2", List.of(new Author("author2")), List.of(new Editor(null, "editorRole2", "editorName2")), new Genre(null, "genre2"), new Publisher(null, "publisher2", "publisherPostalCode2", "publihserAddress2"), new PrintingHouse(null, "printingHouse2", "printingHousePostalCode2", "printingHouseAddress2"), 1998, "Minsk", "description2", 230, "3298614230153", "81.7", "23.4", "B2", 9, true));
 
-        Book thirdBook = new Book(null, "book3", List.of(new Author("author3")), List.of(new Editor(null, "editorRole3", "editorName3")), new Genre(null, "genre3"), new Publisher(null, "publisher3", "publisherPostalCode3", "publihserAddress3"), new PrintingHouse(null, "printingHouse3", "printingHousePostalCode3", "printingHouseAddress3"), 1995, "Minsk", "description3", 60, "3123614390153", "70.7", "33.6", 8, false);
+        Book thirdBook = new Book(null, "book3", List.of(new Author("author3")), List.of(new Editor(null, "editorRole3", "editorName3")), new Genre(null, "genre3"), new Publisher(null, "publisher3", "publisherPostalCode3", "publihserAddress3"), new PrintingHouse(null, "printingHouse3", "printingHousePostalCode3", "printingHouseAddress3"), 1995, "Minsk", "description3", 60, "3123614390153", "70.7", "33.6", "B3", 8, false);
         entityManager.persist(thirdBook);
 
         entityManager.flush();
@@ -86,11 +86,11 @@ public class BookRepositoryTest {
     @Test
     public void testFindAllByDeletedFalseShouldReturnAnEmptyListWhenSuchBooksDoNotExist() {
         //given
-        entityManager.persist(new Book(null, "book1", List.of(new Author("author1")), List.of(new Editor(null, "editorRole1", "editorName1")), new Genre(null, "genre1"), new Publisher(null, "publisher1", "publisherPostalCode1", "publihserAddress1"), new PrintingHouse(null, "printingHouse1", "printingHousePostalCode1", "printingHouseAddress1"), 2003, "Minsk", "description1", 20, "3298614390153", "80.7", "33.4", 12, true));
+        entityManager.persist(new Book(null, "book1", List.of(new Author("author1")), List.of(new Editor(null, "editorRole1", "editorName1")), new Genre(null, "genre1"), new Publisher(null, "publisher1", "publisherPostalCode1", "publihserAddress1"), new PrintingHouse(null, "printingHouse1", "printingHousePostalCode1", "printingHouseAddress1"), 2003, "Minsk", "description1", 20, "3298614390153", "80.7", "33.4", "B1", 12, true));
 
-        entityManager.persist(new Book(null, "book2", List.of(new Author("author2")), List.of(new Editor(null, "editorRole2", "editorName2")), new Genre(null, "genre2"), new Publisher(null, "publisher2", "publisherPostalCode2", "publihserAddress2"), new PrintingHouse(null, "printingHouse2", "printingHousePostalCode2", "printingHouseAddress2"), 1998, "Minsk", "description2", 230, "3298614230153", "81.7", "23.4", 9, true));
+        entityManager.persist(new Book(null, "book2", List.of(new Author("author2")), List.of(new Editor(null, "editorRole2", "editorName2")), new Genre(null, "genre2"), new Publisher(null, "publisher2", "publisherPostalCode2", "publihserAddress2"), new PrintingHouse(null, "printingHouse2", "printingHousePostalCode2", "printingHouseAddress2"), 1998, "Minsk", "description2", 230, "3298614230153", "81.7", "23.4", "B2", 9, true));
 
-        entityManager.persist(new Book(null, "book3", List.of(new Author("author3")), List.of(new Editor(null, "editorRole3", "editorName3")), new Genre(null, "genre3"), new Publisher(null, "publisher3", "publisherPostalCode3", "publihserAddress3"), new PrintingHouse(null, "printingHouse3", "printingHousePostalCode3", "printingHouseAddress3"), 1995, "Minsk", "description3", 60, "3123614390153", "70.7", "33.6", 8, true));
+        entityManager.persist(new Book(null, "book3", List.of(new Author("author3")), List.of(new Editor(null, "editorRole3", "editorName3")), new Genre(null, "genre3"), new Publisher(null, "publisher3", "publisherPostalCode3", "publihserAddress3"), new PrintingHouse(null, "printingHouse3", "printingHousePostalCode3", "printingHouseAddress3"), 1995, "Minsk", "description3", 60, "3123614390153", "70.7", "33.6", "B3", 8, true));
 
         entityManager.flush();
         //when

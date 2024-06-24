5dd9e4f694f00b1651ca4518303364183f516824
Johanot <pavelkrafft@gmail.com>
2024-06-24 11:23:35 +0300

    added security config for password changing

--- src/main/java/com/patiun/libraryspring/configuration/RestSecurityConfig.java
+++ src/main/java/com/patiun/libraryspring/configuration/RestSecurityConfig.java
@@ -50,6 +50,7 @@ public class RestSecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/orders/**").hasAuthority(PLACE_ORDERS.name())
                         .requestMatchers(HttpMethod.GET, BOOKS_URL_PATTERN).permitAll()
                         .requestMatchers(HttpMethod.GET, "/users/auth").authenticated()
+                        .requestMatchers(HttpMethod.PATCH, "/users/*/change-password").authenticated()
                         .requestMatchers(HttpMethod.GET, "/users/**").hasAuthority(READ_USERS.name())
                         .requestMatchers(HttpMethod.GET, "/orders/**").hasAuthority(READ_ORDERS.name())
                         .requestMatchers(HttpMethod.PUT, BOOKS_URL_PATTERN).hasAuthority(EDIT_BOOKS.name())
